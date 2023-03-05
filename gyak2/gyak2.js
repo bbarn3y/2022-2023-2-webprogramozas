d192f7607b47325c9bee0cf7bba03aed8639f93b
bbarn3y <babuafi@inf.elte.hu>
2023-03-05 10:15:39 +0100

    2. gyakorlat starter kit

---
+++ gyak2/gyak2.js
@@ -0,0 +1,61 @@
+// Array functions
+let customers = [
+    {
+        name: 'Ábrahám Erik',
+        time: 1677934024666,
+        cost: 3900,
+        products: [
+            { name: 'Greek salad', type: 'Food' }
+        ]
+    },
+    {
+        name: 'Bodri Balázs',
+        time: 1677764861000,
+        cost: 13500,
+        products: [
+            { name: 'Bottled water', type: 'Drink' },
+            { name: 'Garlic cream-soup', type: 'Food' },
+            { name: 'Wine', type: 'Drink' },
+            { name: 'Beef stew', type: 'Food' }
+        ]
+    },
+    {
+        name: 'Bozsoky Milán Ferenc',
+        time: 1675316861000,
+        cost: 2100,
+        products: [
+            { name: 'Beer', type: 'Drink' },
+            { name: 'Beer', type: 'Drink' },
+            { name: 'Beer', type: 'Drink' }
+        ]
+    },
+    {
+        name: 'Farkas Csobán István',
+        time: 1677764861000,
+        cost: 5200,
+        products: [
+            { name: 'Székelygoulash', type: 'Food' }
+        ]
+    },
+    {
+        name: 'Frigyes Kristóf',
+        time: 1676008061000,
+        cost: 16000,
+        products: [
+            { name: 'Bottled water', type: 'Drink' },
+            { name: 'Paella Valenciana', type: 'Food' },
+            { name: 'Sangria', type: 'Drink' }
+        ]
+    },
+    {
+        name: 'Gortva István',
+        time: 1676267261000,
+        cost: 15500,
+        products: [
+            { name: 'Gazpacho', type: 'Food' },
+            { name: 'Pike-pearch fried in breadcrumbs', type: 'Food' },
+            { name: 'Sangria', type: 'Drink' }
+        ]
+    }
+]
+
---
+++ gyak2/hf1/hf1.js
@@ -0,0 +1,73 @@
+let persons = [
+    {
+        name: 'Horogszegi Richárd Pál',
+        age: 42,
+        interests: [
+            'Békaboncolás',
+            'Futás'
+        ]
+    },
+    {
+        name: 'Kecskés Dávid',
+        age: 32,
+        interests: [
+            'Cosplay',
+            'Fényképezés',
+            'Úszás'
+        ]
+    },
+    {
+        name: 'Jóna Kristóf',
+        age: 15,
+        interests: [
+            'Sorozatnézés',
+            'Sörfőzés'
+        ]
+    },
+    {
+        name: 'Herteg Gabriel Benjamin',
+        age: 19,
+        interests: [
+            'Futás',
+            'Origami',
+            'Úszás'
+        ]
+    },
+    {
+        name: 'Kámán Rebeka Petra',
+        age: 21,
+        interests: [
+            'Bélyeggyűjtés',
+            'LEGO',
+            'Tenyérjóslás',
+            'Zártörés'
+        ]
+    },
+    {
+        name: 'Ji Yu Chen',
+        age: 25,
+        interests: [
+            'Paintball',
+            'Úszás'
+        ]
+    }
+]
+
+// 1.2 - Személynevek ABC rendben
+
+
+// 1.3 - A legfeljebb 25 éves személyek listája
+
+
+// 1.4 - Legöregebb személy
+
+
+// 1.5 - Átlagéletkor
+
+
+// 1.6 - A legtöbb hobbival rendelkező személy
+
+
+// 1.7 - Leggyakrabban előforduló érdeklődési kör
+
+
