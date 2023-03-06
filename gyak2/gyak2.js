bc2efdc3697a3deb26b12d63c0e5bb8bac4bd7a8
bbarn3y <babuafi@inf.elte.hu>
2023-03-06 17:25:24 +0100

    2. gyakorlat

--- gyak2/gyak2.js
+++ gyak2/gyak2.js
@@ -59,3 +59,102 @@ let customers = [
     }
 ]
 
+function spentAtLeast(array, threshold) {
+    return array.filter((a) => a.cost >= threshold);
+}
+
+function customerNames(customers) {
+    return customers.map((b) => b.name);
+}
+
+function splitCustomerNames(tomb) {
+    return tomb.flatMap((person) => person.name.split(' '))
+}
+
+function uniqueSplitCustomerNames(c) {
+    return new Set(splitCustomerNames(c));
+}
+
+function everyoneSpentAtLeast(persons, threshold) {
+    return persons.every((person) => person.cost >= threshold);
+}
+
+function someoneSpentAtLeast(persons, threshold) {
+    return persons.some((person) => person.cost >= threshold);
+}
+
+function namesThatSpentAtLeast(persons, threshold) {
+    return persons
+        .filter((p) => p.cost >= threshold)
+        .map((p) => p.name)
+}
+
+function cummulativeCost(persons) {
+    return persons
+        .reduce(
+            (partialResult, person) =>  partialResult + person.cost,
+            0
+        );
+}
+
+function cummulativeCostForeach(persons) {
+    let result = 0;
+    persons.forEach((person) => {
+        result += person.cost;
+    })
+    return result;
+}
+
+console.log(
+    spentAtLeast(customers, 10000),
+    spentAtLeast(customers, 100000),
+    customerNames(customers),
+    splitCustomerNames(customers),
+    uniqueSplitCustomerNames(customers),
+    everyoneSpentAtLeast(customers, 1000),
+    everyoneSpentAtLeast(customers, 5000),
+    someoneSpentAtLeast(customers, 5000),
+    someoneSpentAtLeast(customers, 50000),
+    namesThatSpentAtLeast(customers, 5000),
+    cummulativeCost(customers),
+    cummulativeCostForeach(customers)
+)
+
+
+
+
+
+// DOM
+
+const button = document.querySelector('#numberSubmitButton');
+const input = document.querySelector('#numberInput');
+const resultDiv = document.querySelector('#numberResult');
+console.log(button, input)
+
+button.addEventListener('click', (event) => {
+    console.log('clicky', event, input.value);
+
+    resultDiv.innerHTML = `
+        <div>
+            <p>
+                ${Math.sqrt(+input.value)}
+            </p>
+        </div>
+    `;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
