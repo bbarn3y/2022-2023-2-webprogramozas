82d8cdfd5b59273c6cb480e68353f27b5d66f8d7
bbarn3y <babuafi@inf.elte.hu>
2023-03-20 17:32:13 +0100

    Gyak 4

--- gyak4/index.js
+++ gyak4/index.js
@@ -15,4 +15,28 @@ const btnRestart = document.querySelector("#restart");
 
 // ========= Solution =========
 
+const randomIndex = random(0, wordList.length - 1);
+const word = wordList[randomIndex];
+
+divTheWord.innerHTML = word;
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  spanError.innerHTML = '';
+
+  if (inputGuess.value.length !== 5) {
+    spanError.innerHTML = 'The length of the word is not 5!';
+    return;
+  }
+
+  if (!wordList.includes(inputGuess.value)) {
+    spanError.innerHTML = 'The word is not considered acceptable!'
+  }
+
+  inputGuess.value.split('').reduce((partialResult, nextLetter, currentIndex) => {
+
+  })
+})
+
 inputGuess.select();
