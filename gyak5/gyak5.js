673b6ebc2fced5af240790bc10f0d38718adc011
bbarn3y <babuafi@inf.elte.hu>
2023-03-27 17:39:12 +0200

    Gyak 5

--- gyak5/gyak5.js
+++ gyak5/gyak5.js
@@ -19,24 +19,74 @@ const solution = randomNumber(1, 100);
 console.log('solution', solution);
 
 const containerEl = document.getElementById('container');
+let adversarysInterval;
 
 function adversaryGuess() {
+    const availableNumberElements = [...document.querySelectorAll('.cell')]
+        .filter((cellEl) => cellAvailable(cellEl))
+        // .map((cellEl) => +cellEl.innerText)
 
+    console.log('availableNumbers', availableNumberElements);
+
+    const adversarysGuessEl = availableNumberElements[randomNumber(0, availableNumberElements.length - 1)]
+
+    if (+adversarysGuessEl.innerText === solution) {
+        adversarysGuessEl.classList.add('adversary-won');
+        if (adversarysInterval) {
+            window.clearInterval(adversarysInterval);
+        }
+        gameOver = true;
+        return;
+    }
+
+    adversarysGuessEl.classList.add('adversary-wrong-guess');
 }
 
-function cellAvailable(target) {
-
+function cellAvailable(cell) {
+    return !cell.classList.contains('player-wrong-guess') &&
+        !cell.classList.contains('player-wins') &&
+        !cell.classList.contains('adversary-won') &&
+        !cell.classList.contains('adversary-wrong-guess');
 }
 
 function createGrid() {
 
+
+    const html = [...new Array(10).keys()].map((rowNumber) => {
+        return [...new Array(10).keys()].map((colNumber) => {
+            return `<div class="cell">${rowNumber * 10 + colNumber + 1}</div>`
+        }).join('');
+    }).join('<br>')
+
+    containerEl.innerHTML = html;
 }
 
 createGrid();
 
-// delegate(, '', '', (event) => {
-//
-// });
+delegate(containerEl, 'click', 'div.cell', function (event) {
+    if (!cellAvailable(this) || gameOver) {
+        return;
+    }
+
+    const number = +this.innerText;
+
+    console.log('number', number);
+
+    if (number === solution) {
+        this.classList.add('player-wins');
+        if (adversarysInterval) {
+            window.clearInterval(adversarysInterval);
+        }
+        gameOver = true;
+        return;
+    }
+
+    this.classList.add('player-wrong-guess');
+});
+
+adversarysInterval = window.setInterval(() => {
+    adversaryGuess();
+}, 5000);
 
 
 
