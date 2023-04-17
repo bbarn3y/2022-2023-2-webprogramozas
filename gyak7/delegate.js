abdcadbc84f51839343409b4cbf5fc70e762bf61
bbarn3y <babuafi@inf.elte.hu>
2023-04-17 19:16:55 +0200

    Gyak 7

--- gyak7/delegate.js
+++ gyak7/delegate.js
@@ -8,7 +8,10 @@ const task8El = document.getElementById('task8');
 const task9EL = document.getElementById('task9');
 const tasksEl = document.getElementById('tasks');
 
-selectedNumbers = [];
+let selectedGame;
+let rowCount;
+let columnCount;
+let selectedNumbers = [];
 
 function delegate(parent, type, selector, handler) {
     parent.addEventListener(type, function (event) {
@@ -29,4 +32,82 @@ function drawLottery(n){
     return draw.sort((u, v) => u - v)
 }
 
+function generateTable(rowCount, columnCount) {
+    let html = '';
+
+    for (let i = 0; i < rowCount; i++) {
+        html += '<tr>'
+        for (let j = 0; j < columnCount; j++) {
+            html += `<td>${i * columnCount + j + 1}</td>`
+        }
+        html += '</tr>'
+    }
+
+    tableEl.innerHTML = html;
+}
+
+newEl.addEventListener('click', (event) => {
+    newEl.setAttribute('disabled', '');
+    selectEl.setAttribute('disabled', '');
+
+    selectedGame = selectEl.value;
+    switch(selectedGame) {
+        case '5':
+            rowCount = 10;
+            columnCount = 9;
+            break;
+        case '6':
+            rowCount = 5;
+            columnCount = 9;
+            break;
+        case '7':
+            rowCount = 5;
+            columnCount = 7;
+            break;
+    }
+
+    generateTable(rowCount, columnCount);
+});
+
+delegate(tableEl, 'click', 'td', function(event) {
+    const selectNumber = +this.innerText;
+    if (selectedNumbers.includes(selectNumber)) {
+        const index = selectedNumbers.indexOf(selectNumber);
+        selectedNumbers.splice(index, 1);
+    } else {
+        if (selectedNumbers.length === +selectedGame) {
+            return;
+        }
+        selectedNumbers.push(selectNumber);
+    }
+    this.classList.toggle('played');
+
+    if (selectedNumbers.length === +selectedGame) {
+        tasksEl.style.display = 'block';
+    } else {
+        tasksEl.style.display = 'none';
+    }
+})
+
+drawEl.addEventListener('click', (event) => {
+    const winningNumbers = drawLottery(selectedGame);
+    task6El.innerText = winningNumbers.join(', ');
+
+    const matchingNumbers = winningNumbers.filter((winningNumber) => {
+       return selectedNumbers.includes(winningNumber)
+    });
+    task7El.innerText = matchingNumbers.length;
+
+    const joinedNumbers = winningNumbers.join('');
+    const splitNumber = joinedNumbers.split('')
+    const sum = splitNumber.reduce((partialResult, nextNumber) => {
+        return partialResult + +nextNumber;
+    }, 0);
+    task8El.innerText = sum;
+
+    const squareFound = winningNumbers.some((winningNumber) => {
+        return winningNumbers.includes(Math.pow(winningNumber, 2))
+    });
+    task9EL.innerText = squareFound
+});
 
--- gyak7/form.js
+++ gyak7/form.js
@@ -16,19 +16,42 @@ const stateStorageKey = 'state';
 })
 
 formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
 
+    if ([...formEl.elements].every((element) => element.validity.valid)) {
+        save();
+    }
 });
 
 function init() {
+    const stateString = localStorage.getItem(stateStorageKey);
+    if (stateString) {
+        const state = JSON.parse(stateString);
+        availableInputEl.value = state.available;
+        nextInputEl.value = state.next;
+        protagonistInputEl.value = state.protagonist;
+        titleInputEl.value = state.title;
+    }
+
     validate();
 }
 
 function save() {
-
+    const state = {
+        available: availableInputEl.value,
+        next: nextInputEl.value,
+        protagonist: protagonistInputEl.value,
+        title: titleInputEl.value
+    }
+    localStorage.setItem(stateStorageKey, JSON.stringify(state));
 }
 
 function validate() {
-
+    if (availableInputEl.value === 'yes' && !nextInputEl.value) {
+        nextInputEl.setCustomValidity('Ha játszák moziban, akkor töltsd ki a dátumot!');
+    } else {
+        nextInputEl.setCustomValidity('');
+    }
 }
 
 init();
